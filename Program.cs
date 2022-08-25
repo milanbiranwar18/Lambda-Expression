cccaea9392fdc677c824ed9dc39670deaa46e8bb
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-25 13:15:58 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LambdaExpression
+{
+    public class Program
+    {
+        public static void Main(string[]args)
+        {
+            Console.WriteLine("Wellcome to Lambda Expression Problem");
+
+        }
+    }
+}
