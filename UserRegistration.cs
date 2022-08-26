686c6016b086d6f86e7cc1145afa022e2ee4e464
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-26 17:45:43 +0530

    Program for validating last name using lambda expression

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -15,7 +15,10 @@ namespace LambdaExpression
 
 
 
-        
+        //UC2
+         static string RegexLastName = "^[A-Z]{1,}[a-z]{3,}$";
+        public string validateLastNameusingLambda(string LastName) => Regex.IsMatch(LastName, RegexLastName) ? "valid" : "Ivalid";
+
 
     }
 }
