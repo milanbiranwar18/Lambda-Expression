eec761e2b2d9f574ad70db97a1df9679b98cc4ef
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-26 18:33:25 +0530

    Adding program for validating password using lambda expression

--- Program.cs
+++ Program.cs
@@ -93,9 +93,21 @@
             //}
 
 
+            //Console.WriteLine("Enter Lastname");
+            //string password = Console.ReadLine();
+            //string opt = user.validatePasswordusingLambda(password);
+            //if (opt == "valid")
+            //{
+            //    Console.WriteLine("Pattern is matched");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Pattern is not matched");
+            //}
+
             Console.WriteLine("Enter Lastname");
             string password = Console.ReadLine();
-            string opt = user.validatePasswordusingLambda(password);
+            string opt = user.validatePassword6usingLambda(password);
             if (opt == "valid")
             {
                 Console.WriteLine("Pattern is matched");
@@ -104,6 +116,8 @@
             {
                 Console.WriteLine("Pattern is not matched");
             }
+
+            
         }
     }
 }
